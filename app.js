b396b1deea005832a5329806954e8b357e89def6
faiqkhan125 <155578969+faiqkhan125@users.noreply.github.com>
2024-02-22 13:01:46 -0800

    javascript 1st Assignment created
    javascript 1st Assignment created

---
+++ app.js
@@ -0,0 +1,54 @@
+// alert("Hello js");
+// var firstName$ = "Faiq";
+// console.log(typeof(firstName$))
+
+// var age = "20";
+// console.log(typeof(age));
+
+
+// question no 1
+// alert("Error! Please Enter a valid Password");
+
+// question no 2
+// alert("Welcome to JS Land\n Happy Coding!");
+
+// question no 3
+// alert("Welcome to JS Land..")
+
+// question no 4
+// alert("Happy Coding!\n  Prevent this page from creating additional dialogs");
+
+// question no 5
+// alert("Hello... i can run JS through my web browser's console");
+
+// chapter 3
+
+// question no 1
+// var age = "20";
+// alert("my age is 20");
+
+// qustion no 2
+// alert("you have visited this website n times");
+// alert("you have visited this website 1 times");
+// alert("you have visited this website 2 times");
+// alert("you have visited this website 3 times");
+// alert("you have visited this website 4 times");
+// alert("you have visited this website 5 times");
+// alert("you have visited this website 6 times");
+// alert("you have visited this website 7 times");
+// alert("you have visited this website 8 times");
+// alert("you have visited this website 9 times");
+// alert("you have visited this website 10 times");
+// alert("you have visited this website 11 times");
+// alert("you have visited this website 12 times");
+// alert("you have visited this website 13 times");
+// alert("you have visited this website 14 times");
+
+// question no 3
+// var birthyear = "2005";
+// console.log(birthyear);
+
+
+
+
+
